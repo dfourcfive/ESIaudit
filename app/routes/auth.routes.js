cc70b6ddc1778f7bae011f1d20657700efa2117d
dfourcfive <i.benadidou@esi-sba.dz>
2021-05-06 15:32:44 +0100

    authentication added

---
+++ app/routes/auth.routes.js
@@ -0,0 +1,24 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+    //route for singUp
+  app.post(
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted
+    ],
+    controller.signup
+  );
+    //route for singIn
+  app.post("/api/auth/signin", controller.signin);
+};
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./app/models");
+const Role = db.role;
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+db.sequelize.sync().then(() => {
+    console.log('Resync Db');
+});
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to EsiAudit-API application." });
+});
+//routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
